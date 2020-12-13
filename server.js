1998161334b3e243219cbf18a5670f72648e2ad8
ducktrshessami <jwee761@gmail.com>
2020-12-12 16:46:17 -0800

    base web server

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const PORT = process.env.PORT || 5366;
+
+var app = express();
+
+// Pages
+app.get("/", (request, response) => {
+    response.status(200).sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/notes", (request, response) => {
+    response.status(200).sendFile(path.join(__dirname, "public", "notes.html"));
+});
+
+// Assets
+app.get("/assets/:dir/:file", (request, response) => {
+    let file = path.join(__dirname, "public", "assets", request.params.dir, request.params.file);
+    if (fs.existsSync(file)) {
+        response.status(200).sendFile(file);
+    }
+    else {
+        response.status(404).end();
+    }
+});
+
+// Start server
+app.listen(PORT, function() {
+    console.log(`Listening on PORT ${PORT}`);
+});
